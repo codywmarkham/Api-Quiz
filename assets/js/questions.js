a42c8e42c8948998b7404eb5455c1c000bb1c5f5
High_Lander <codywmarkham@gmail.com>
2021-10-04 15:52:08 -0500

    added questions

---
+++ assets/js/questions.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+      title: "Why is the Web API important?",
+      choices: ["For flex boxing", "To make the page pretty", "It is used to provide an interface for websites", "Its not"],
+      answer: "It is used to provide an interface for websites"
+    },
+    {
+      title: "What is Web API ",
+      choices: ["Its the internet", "Its email", "Facebook", "It is a tool that can be used to push data to a server"],
+      answer: "It is a tool that can be used to push data to a server"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    },
+    {
+      title:
+        "What are the main return types supported in Web API?",
+      choices: ["GET, POST, PUT", "Left,Right,UP", "GOT,PAST,PAT", "DID,DO,DONT"],
+      answer: "GET, POST, PUT"
+    },
+    {
+      title:
+        "Web API supports which protocol?",
+      choices: ["JavaScript", "terminal / bash", "HTTP", "HTML"],
+      answer: "HTTP"
+    }
+  ];
+  
